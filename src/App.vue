1c4ed045d6b71a40b81dd54a3420b0b55277d8a2
flynn <769339756@qq.com>
2023-09-27 17:04:54 +0800

    [FIX]初始化脚手架

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <DEMO></DEMO>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import DEMO from './index/demo.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,DEMO
   }
 }
 </script>
